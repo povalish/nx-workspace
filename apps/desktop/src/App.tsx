ad9e64a5892045108e63c696721f47c24a07a2f3
Nikita Povalishev <np@skl.vc>
2023-07-10 22:54:41 +0300

    Setup Electron app

---
+++ apps/desktop/src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div style={{ backgroundColor: 'red' }}>
+      <h1 style={{ color: '#ffffff' }}>Hello from Electron!</h1>
+    </div>
+  );
+};
+
+export default App;
