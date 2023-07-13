e5fbeccc58d6dd8314e5b77b4d162e9e891e9e98
Nikita Povalishev <np@skl.vc>
2023-07-14 00:54:59 +0300

    Generate empty ReactNative app for Storybook

---
+++ apps/storybook-native/src/app/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { SafeAreaView, StatusBar, Text } from 'react-native';
+
+//
+//
+
+export const App = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <Text>Storybook instructions</Text>
+      </SafeAreaView>
+    </>
+  );
+};
+
+export default App;
