e3ed6fd41d7bac162d962b9bc563b50764f5b86b
Nikita Povalishev <np@skl.vc>
2023-07-14 00:07:38 +0300

    Generate Storybook app for React components

---
+++ apps/storybook-react/src/components/Button.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Button } from '@ui-kit/components/button/Button'
+
+// Config
+//
+
+const meta: Meta<typeof Button> = {
+  component: Button,
+  title: 'Components/Button'
+};
+
+export default meta;
+type Story = StoryObj<typeof Button>;
+
+// Stories
+//
+
+export const ButtonComponent: Story = {
+  render: () => <Button onPress={() => console.log('Clicked!')}>Submit</Button>,
+};
